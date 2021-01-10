7eb68845e07afb41c3f5a97d123a3ce3350c563d
Adam Friedman <afriedman@bignerdranch.com>
2021-01-09 22:52:20 -0800

    Update footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,11 +2,11 @@ import "../Footer/footer.css";
 
 export default function Footer(props) {
   return (
-    <footer class="footer-box">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <p class="footer-company-name">
+    <footer className="footer-box">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <p className="footer-company-name">
               All Rights Reserved. &copy; 2020 Designed By: Adam Friedman
             </p>
           </div>
