224d5d13e639efe976c28460041813580c548bab
Adam Friedman <adam.friedman5@gmail.com>
2021-05-02 20:54:05 -0700

    Update hotels

--- src/Components/Travel/Travel.jsx
+++ src/Components/Travel/Travel.jsx
@@ -38,45 +38,23 @@ export default function Travel(props) {
 
           <div className="col-lg-4 col-md-6 col-sm-12 service-card">
             <div className="serviceBox">
-              <h3 className="title">Hotels Nearby</h3>
-              <div className="row">
-                <p className="description col-md-6">
-                  <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://www.wyndhamhotels.com/wyndham-grand/orlando-florida/wyndham-grand-orlando-resort-bonnet-creek/overview?CID=LC:GR::GGL:RIO:National:45462&iata=00093796"
-                  >
-                    Bonnet Creek
-                  </a>
-                  <br />
-                  <span className="addressText">
-                    14651 Chelonia Pkwy <br />
-                    Orlando, FL 32821
-                  </span>
-                  <br />
-                  <span className="distanceText">
-                    (Approx. 41 min to venue)
-                  </span>
-                </p>
-                <p className="description col-md-6">
-                  <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://www.hilton.com/en/hotels/sfbakgi-hilton-garden-inn-apopka-city-center/"
-                  >
-                    Hilton Garden Inn
-                  </a>
-                  <br />
-                  <span className="addressText">
-                    580 E. Main Street <br />
-                    Apopka, Fl 32703
-                  </span>
-                  <br />
-                  <span className="distanceText">
-                    (Approx. 11 min to venue)
-                  </span>
-                </p>
-              </div>
+              <h3 className="title">Closest to Venue</h3>
+              <p className="description">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://www.hilton.com/en/hotels/sfbakgi-hilton-garden-inn-apopka-city-center/"
+                >
+                  Hilton Garden Inn
+                </a>
+                <br />
+                <span className="addressText">
+                  580 E. Main Street <br />
+                  Apopka, Fl 32703
+                </span>
+                <br />
+                <span className="distanceText">(Approx. 11 min to venue)</span>
+              </p>
             </div>
           </div>
 
